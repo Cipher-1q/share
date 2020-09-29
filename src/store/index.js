a380e7fa673fb26be287e773d6bfd4ffc29e5adf
Cipher-1q <mhethihethir524@ezweb.ne.jp>
2020-09-29 10:23:10 +0900

    Initial; Commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import axios from "axios";
+import router from "../router/index";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    auth: "",
+    user: "",
   },
   mutations: {
+    auth(state, payload) {
+      state.auth = payload;
+    },
+    user(state, payload) {
+      state.auth = payload;
+    },
+    logout(state, payload) {
+      state.auth = payload;
+    },
+    changeUserData(state, payload) {
+      state.user.profile = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
